7fe3d767fdb1042c89bbd29cb698905970a5b52c
Abdirahman Abdiaziz <a.abdiaziz@hotmail.co.uk>
2023-04-04 23:17:23 +0300

    I finished the modal. then moved on to inputting values and dynamically placing them inside a p tag.

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+//timer variables
 let timerText = document.getElementById("timer")
 let startGame = document.getElementById("start-game")
 let stopGame = document.getElementById("stop-game")
 
+//scoreboard variables
 let scoreBtn1 = document.getElementById("point-action-home")
 let scoreBtn2 = document.getElementById("point-action-away")
 
+//modal open and close variable
+let modalBtn = document.getElementById("player-action-home")
+let modalBg = document.querySelector(".modal-bg")
+let activeBG = document.querySelector(".bg-active")
+let spanClose = document.getElementById("close")
+let submitBtn = document.getElementById("submit-btn")
+
+//updating match stats variables
+let matchStats = document.getElementById("player")
 
 startGame.addEventListener("click", () => { 
     let seconds = 0
@@ -23,7 +34,7 @@ startGame.addEventListener("click", () => {
 
 
 
-
+//score
 
 
 let count = 0
@@ -39,3 +50,34 @@ scoreBtn2.addEventListener("click", () => {
     score2.innerHTML = count2
     console.log(score2) 
 });
+
+// Adding action e.g. Red/yellow card or warning.
+
+modalBtn.addEventListener("click", () => {
+    modalBg.classList.toggle("bg-active")
+})
+spanClose.addEventListener("click", () => {
+    modalBg.classList.toggle("bg-active")
+})
+submitBtn.addEventListener("click", () => {
+    modalBg.classList.toggle("bg-active")
+    // console.log("yeye")
+})
+
+function getFormData () {
+    let playerNumber = document.getElementById("player-input").value
+    let playerAction = document.getElementById("player-action").value
+
+
+    console.log(player =
+        {
+            "player" : {
+                "number" : playerNumber, 
+                "action" : playerAction
+            }
+        })
+
+        matchStats.innerHTML = player[1]
+}
+
+console.log(getFormData())
