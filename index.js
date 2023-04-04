7e44875f79a4f9d6bb039d2cef818dd5a929430d
Abdirahman Abdiaziz <a.abdiaziz@hotmail.co.uk>
2023-04-04 15:41:19 +0300

    added two buttons and hooked one of them to the onclick event, now the game starts at the click of the button

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 let timerText = document.getElementById("timer")
+let startGame = document.getElementById("start-game")
+
 let scoreBtn1 = document.getElementById("point-action-home")
 let scoreBtn2 = document.getElementById("point-action-away")
-let score1 = document.getElementById("score1")
 
-addEventListener.on () { 
+
+startGame.addEventListener("click", () => { 
     let seconds = 0
     timer = setInterval(() => {
-        // timerText.innerHTML = 0 + seconds;
         seconds ++;
-        // console.log(timerText.innerHTML)
+
         let minutes = Math.floor(seconds / 60);
         let extraSeconds = seconds % 60;
+
         minutes = minutes < 10 ? "0" + minutes : minutes;
         extraSeconds = extraSeconds < 10 ? "0" + extraSeconds : extraSeconds;
-        // console.log(minutes + "" + extraSeconds)
+
         timerText.textContent = minutes + ":"+ extraSeconds
     }, 1000);
-}
+})
 
-secondsCalc()
 
 let count = 0
 let count2 = 0
@@ -29,6 +30,7 @@ scoreBtn1.addEventListener("click", () =>{
 });
 
 scoreBtn2.addEventListener("click", () => {
-    count += 1
-    score2.innerHTML = count 
+    count2 += 1
+    score2.innerHTML = count2
+    console.log(score2) 
 });
